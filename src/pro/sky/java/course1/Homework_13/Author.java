85d8bfd4dc90ec1c33c81e3b9cc154e9902a6be4
Yuna <ksedih@rambler.ru>
2023-08-06 05:46:37 +0300

    Homework_13 was done

--- src/pro/sky/java/course1/Homework_13/Author.java
+++ src/pro/sky/java/course1/Homework_13/Author.java
@@ -1,4 +1,34 @@
 package pro.sky.java.course1.Homework_13;
 
+import java.util.Objects;
+
 public class Author {
+    private String firstName;
+    private String surname;
+
+    public Author(String firstName, String surname) {
+        this.firstName = firstName;
+        this.surname = surname;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    @Override
+    public String toString() {
+        return firstName + " " + surname;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, surname);
+    }
+    @Override
+    public boolean equals(Object i) {
+        if (this == i) return true;
+        if (i == null || getClass() != i.getClass()) return false;
+        Author author = (Author) i;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(surname, author.surname);
+    }
 }
--- src/pro/sky/java/course1/Homework_13/Main.java
+++ src/pro/sky/java/course1/Homework_13/Main.java
@@ -1,5 +1,27 @@
+import pro.sky.java.course1.Homework_13.Author;
+import pro.sky.java.course1.Homework_13.Book;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author steinbeck = new Author("Джон", "Стейнбек");
+        Author keyes = new Author("Дэниел", "Киз");
+        Author orwell = new Author("Джордж", "Оруэлл");
+
+        Book ofMiceAndMen = new Book("О мышах и людях", steinbeck, 1937);
+        Book flowersForAlgernon = new Book("Цветы для Элджернона", keyes, 1966);
+        Book nineteenEightyFour = new Book("1984", orwell, 1956);
+
+        printBook(flowersForAlgernon);
+
+        System.out.println(ofMiceAndMen.equals(nineteenEightyFour));
+        System.out.println(ofMiceAndMen.equals(flowersForAlgernon));
+
+
+    }
+    private static void printBook(Book book) {
+        System.out.println(book.toString());
     }
 }
+
+
+
